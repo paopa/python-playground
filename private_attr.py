2358a28744e95e8254f0bab19fa8ff2cd1443598
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-16 02:43:36 +0800

    experimented with private attributes and access control in Python using the Demo class

---
+++ private_attr.py
@@ -0,0 +1,33 @@
+class Demo:
+    def __init__(self, value):
+        self.__value = value
+
+    def get_value(self):
+        return self.__value
+
+    def set_value(self, value):
+        self.__value = value
+
+
+if __name__ == "__main__":
+    obj = Demo(42)
+    print(obj.get_value())
+    obj.set_value(43)
+    print(obj.get_value())
+    obj.__value = 44  # this will create a new variable __value in the object
+    # print(obj.__value)
+    print(obj._Demo__value)
+    obj._Demo__value = 44
+    print(obj.get_value())
+    print(obj.__dict__)
+    '''
+    Python have a convention to use a single underscore prefix for a variable to indicate that it is private.
+    However, this is just a convention and does not actually make the variable private.
+    so, we can access the private variable using the _<class_name>__<variable_name> syntax.
+    
+    in the Python community, it is generally considered bad practice to use this syntax to access private variables.
+    and it is better to use the public methods to access and modify the private variables.
+    
+    we're all adults here, and we can make our own decisions.
+    this slogan is often used in the Python community to indicate that Python developers are trusted to make their own decisions.
+    '''
