fd9764945b502806f6141dad9c51c29881d0d6e1
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-17 02:30:22 +0800

    demonstrate private attributes via the property class

--- property.py
+++ property.py
@@ -22,12 +22,13 @@ class Demo:
     '''
     the property getter method shares the same name as the prefix of the setter method.
     '''
+
     @age_x.setter
     def age_x(self, age):
         self.__age = age
 
 
-if __name__ == "__main__":
+def test_demo():
     demo = Demo("John", 28)
     print(demo.name)
     demo.name = "Alex"
@@ -42,3 +43,45 @@ if __name__ == "__main__":
     print(demo.age_x)
     demo.age_x = 30
     print(demo.age_x)
+
+
+class Tree:
+    def __init__(self, breed: str, age: int):
+        self.__breed = breed
+        self.__age = age
+
+    def __get_breed(self) -> str:
+        return self.__breed
+
+    def __get_age(self) -> int:
+        return self.__age
+
+    def __set_age(self, age: int):
+        if age > 15000 or age < 0:
+            raise Exception('the age does not make sense')
+        self.__age = age
+
+    def __del_age(self):
+        del self.__age
+
+    breed = property(fget=__get_breed)
+    age = property(fget=__get_age, fset=__set_age, fdel=__del_age)
+
+
+def test_tree():
+    try:
+        tree = Tree('cedar', 150)
+        print(f'{tree.breed=}')
+        # tree.breed = 'oak' # AttributeError: can't set attribute
+        print(f'aa: {tree.age=}')
+        tree.age = -100
+        print(f'bb: {tree.age=}')
+    except Exception as e:
+        print(f'error：{str(e)}')
+    finally:
+        print(f'cc: {tree.age=}')
+
+
+if __name__ == "__main__":
+    # test_demo()
+    test_tree()
