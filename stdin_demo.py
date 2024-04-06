dfc603573b653e6f644f97e98e75523d27ede29c
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-04-06 22:04:45 +0800

    Read Input From stdin in Python using input()

--- stdin_demo.py
+++ stdin_demo.py
@@ -1,10 +1,25 @@
 import sys
 
-print("Enter 'q' to exit:")
-for line in sys.stdin:
-    if "q" == line.rstrip():
-        break
-    print(f"Input : {line}")
-    print("Enter 'q' to exit:")
 
-print("Exit")
+def demo_sys_stdin():
+    print("Enter 'q' to exit:")
+    for line in sys.stdin:
+        if "q" == line.rstrip():
+            break
+        print(f"Input : {line}")
+        print("Enter 'q' to exit:")
+
+    print("Exit")
+
+def demo_input():
+    while True:
+        line = input("Enter 'q' to exit: ")
+        if "q" == line:
+            break
+        print(f"Input : {line}")
+    print("Exit")
+
+
+if __name__ == "__main__":
+    # demo_sys_stdin()
+    demo_input()
