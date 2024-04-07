704eb74af1740e8120792376a1848b4be7b23706
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-04-07 22:10:17 +0800

    parse json array

--- json_lib.py
+++ json_lib.py
@@ -7,5 +7,14 @@ def test_json_str_with_newline():
     print(json_str)
 
 
+def test_parse_json_array():
+    text = '[{"sql": "Hello World!"}, {"sql": "SELECT 1"}]'
+    res = json.loads(text)
+    for r in res:
+        print(r)
+        print(r["sql"])
+
+
 if __name__ == "__main__":
-    test_json_str_with_newline()
+    # test_json_str_with_newline()
+    test_parse_json_array()
