de3d5220f88314d6d997f081f284ac9a70b05a1d
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-05-07 22:14:57 +0800

    try: logging

--- log/log.py
+++ log/log.py
@@ -1,7 +1,13 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.setLevel(logging.DEBUG)
 
+logging.debug("This is a debug message")
 logging.warning("Watch out!")  # will print a message to the console
 logging.info("I told you so")  # will not print anything
+
+logger = logging.getLogger("demo")
+logger.info("This is a custom logger message")
+logger.error("This is an error message")
+logger.warning("This is a warning message")
+logger.debug("This is a debug message")
