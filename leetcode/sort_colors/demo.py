b136dbab794374fcaf5486ef43bfea4c56bc0cb9
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-06-19 10:29:16 +0800

    leetcode: sort colors

---
+++ leetcode/sort_colors/demo.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def sortColors(self, nums):
+        red, white, blue = 0, 0, len(nums) - 1
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                red += 1
+                white += 1
+                continue
+
+            if nums[white] == 1:
+                white += 1
+                continue
+
+            if nums[white] == 2:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+
+
+nums = [2, 0, 2, 1, 1, 0]
+Solution().sortColors(nums)
+print(nums)
+
+nums = [2, 0, 1]
+Solution().sortColors(nums)
+print(nums)
+
+nums = [1, 0, 2, 0, 1, 0]
+Solution().sortColors(nums)
+print(nums)
