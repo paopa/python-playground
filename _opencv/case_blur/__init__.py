bd49c48dd5b1ddd12568a58b59503112018336b7
Pao Sheng <paooap.oappao@gmail.com>
2025-02-24 23:41:43 +0800

    chore: move blur case to an independent folder

---
+++ _opencv/case_blur/__init__.py
@@ -0,0 +1,30 @@
+import numpy as np
+import skimage
+import cv2
+
+image = skimage.data.chelsea()
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# 7x7 blur filter
+dim = (7, 7)
+blur_filter = np.ones(dim, dtype=np.float32) / np.prod(dim)
+
+blurred_image = cv2.filter2D(image, -1, blur_filter)
+
+cv2.imshow("Blurred Image", blurred_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# 21x21 blur filter
+dim = (21, 21)
+blur_filter = np.ones(dim, dtype=np.float32) / np.prod(dim)
+
+blurred_image = cv2.filter2D(image, -1, blur_filter)
+
+cv2.imshow("Blurred Image", blurred_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
